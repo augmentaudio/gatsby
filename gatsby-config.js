a7e327cad583ebbf1cf17e866544e39e1432c331
jonportelli <jonportelli@gmail.com>
2018-05-18 19:15:49 +0200

    form netlify tag new

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Dimension by HTML5 UP"
+    title: "Jonathan Portelli :: Design Portfolio",
+    author: "Jonathan Portelli",
+    description: "A personal portfolio site built with Gatsby React"
   },
   pathPrefix: '/',
   plugins: [
