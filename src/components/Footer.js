5d82069b8fafd3eeb221260c1f6ca8c87eec6a76
dev <dev@devs-Mac-Pro.local>
2018-09-08 15:59:09 +0200

    favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; React JS Design - Design & Built by: <a href="https://www.jonathanportelli.com/">Jonathan Portelli</a></p>
+        <p className="copyright">&copy; Design & Built by: <a href="https://www.jonathanportelli.com/">Jonathan Portelli</a></p>
     </footer>
 )
 
